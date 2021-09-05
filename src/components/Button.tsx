e70aea446a7faf2eb8358303b9c0f2851880fac9
Derrick Hawkins <dhawkvq@yahoo.com>
2021-09-04 23:16:50 -0700

    Button needed className passed down to extend styles of component.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,18 +5,20 @@ import styled from "styled-components";
  * @param {string} [buttonText="Submit"] = Defaults to Submit
  */
 
-export const Button: FC<{ buttonText?: string; onClick?(): unknown }> = ({
-  buttonText = "Submit",
-  onClick,
-}) => {
-  return <ThemedButton onClick={onClick}>{buttonText}</ThemedButton>;
-};
+export const Button: FC<{
+  buttonText?: string;
+  onClick?(): unknown;
+  className?: string;
+}> = ({ buttonText = "Submit", onClick, className }) => (
+  <ThemedButton onClick={onClick} className={className}>
+    {buttonText}
+  </ThemedButton>
+);
 
 const ThemedButton = styled.button`
   background-color: #6262ff;
   padding: 10px 30px;
-  border-color: #00008b;
-  border: 1px solid;
+  border: none;
   border-radius: 5px;
   color: white;
   cursor: pointer;
