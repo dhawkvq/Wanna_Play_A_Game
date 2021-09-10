cccfa8f12ae2691309564c615ac3071afa5e952d
Derrick Hawkins <dhawkvq@yahoo.com>
2021-09-09 22:25:50 -0700

    users ranked and with a real rough leaderboard.

--- src/pages/LeaderBoard.tsx
+++ src/pages/LeaderBoard.tsx
@@ -24,27 +24,25 @@ const UserStats: FC<{ user: User }> = ({ user }) => (
       alignContent: "center",
     }}
   >
-    {/* users picture */}
-    <div style={{ height: 100, width: 100 }}>
-      <img
-        src={user.avatarURL}
-        alt="avatar"
-        height="100%"
-        width="100%"
-        style={{ borderRadius: 8 }}
-      />
-    </div>
-    {/* users name */}
+    {/* users picture/ name */}
     <div
       style={{
-        marginLeft: 10,
-        minHeight: "100%",
-        flex: 3,
-        fontSize: 18,
-        fontWeight: "bold",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
         color: "black",
+        fontWeight: "bold",
       }}
     >
+      <div style={{ height: 100, width: 100 }}>
+        <img
+          src={user.avatarURL}
+          alt="avatar"
+          height="100%"
+          width="100%"
+          style={{ borderRadius: 8 }}
+        />
+      </div>
       {user.name}
     </div>
     {/* questions asked */}
@@ -52,8 +50,6 @@ const UserStats: FC<{ user: User }> = ({ user }) => (
       style={{
         marginLeft: 10,
         borderLeft: "1px solid black",
-        // display: "flex",
-        // height: "100%",
         flex: 1,
       }}
     >
@@ -69,13 +65,28 @@ const UserStats: FC<{ user: User }> = ({ user }) => (
         textAlign: "center",
       }}
     >
-      {user.answers?.length ?? 0}
+      {Object.keys(user.answers ?? {}).length}
     </div>
   </div>
 );
 
 export const LeaderBoard: FC = () => {
   const currentUsers = useReduxState(({ allUsers }) => allUsers);
+
+  const getUserTotal = (user: User) => {
+    const answerCount = Object.keys(user.answers ?? {}).length;
+    const questionCount = user.questions?.length ?? 0;
+    return answerCount + questionCount;
+  };
+
+  const rankedUsers = Object.entries(currentUsers).sort(
+    ([aKey, aUser], [bKey, bUser]) => {
+      const userATotal = getUserTotal(aUser);
+      const userBTotal = getUserTotal(bUser);
+      return userATotal > userBTotal ? -1 : 1;
+    }
+  );
+
   return (
     <div
       style={{
@@ -89,11 +100,33 @@ export const LeaderBoard: FC = () => {
       }}
     >
       <h1>LeaderBoard!</h1>
-      <>
-        {Object.entries(currentUsers).map(([key, user]) => (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flex: 1,
+            width: "70%",
+            border: "1px dashed blue",
+            margin: "15px 0",
+            alignContent: "center",
+            minHeight: 30,
+          }}
+        >
+          <div style={{ flex: 1 }} />
+          <div style={{ flex: 2 }}>answers</div>
+          <div style={{ flex: 1 }}>questions</div>
+        </div>
+        {rankedUsers.map(([key, user]) => (
           <UserStats key={key} user={user} />
         ))}
-      </>
+      </div>
     </div>
   );
 };
