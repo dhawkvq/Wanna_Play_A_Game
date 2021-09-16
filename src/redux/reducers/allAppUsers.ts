842005c15028be186c93d7130bf311a304847785
Derrick Hawkins <dhawkvq@yahoo.com>
2021-09-15 22:55:31 -0700

    addQuestionToUser and saveUserAnswer actions added to allAppUsers slice

--- src/redux/reducers/allAppUsers.ts
+++ src/redux/reducers/allAppUsers.ts
@@ -1,12 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { UserDb, users } from "../../DB/_DATA";
+import { Question } from "../../types/Question";
+import { SaveAnswersArgs } from "./questions";
 
 const initialState: UserDb = { ...users };
 
 const AllUsersSlice = createSlice({
   name: "allUsers",
   initialState,
-  reducers: {},
+  reducers: {
+    addQuestionToUser: (state, action: PayloadAction<Question>) => {
+      const { authorId, id } = action.payload;
+      const userToUpdate = state[authorId];
+      return {
+        ...state,
+        [authorId]: {
+          ...userToUpdate,
+          questions: [...(userToUpdate.questions ?? []), id],
+        },
+      };
+    },
+    saveUserAnswer: (state, action: PayloadAction<SaveAnswersArgs>) => {
+      const { userId, questionId, option } = action.payload;
+      const userToUpdate = state[userId];
+      return {
+        ...state,
+        [userId]: {
+          ...userToUpdate,
+          answers: {
+            ...userToUpdate.answers,
+            [questionId]: option,
+          },
+        },
+      };
+    },
+  },
 });
+
+export const { addQuestionToUser, saveUserAnswer } = AllUsersSlice.actions;
 
 export const AllUsersReducer = AllUsersSlice.reducer;
