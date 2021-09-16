d882a45ffc2a5fa1ec7a8da7acdc4835d6d6ba40
Derrick Hawkins <dhawkvq@yahoo.com>
2021-09-15 22:57:29 -0700

    crude NewPoll component created. WIP

--- src/pages/NewPoll.tsx
+++ src/pages/NewPoll.tsx
@@ -1,5 +1,58 @@
-import { FC } from "react";
+import { FC, FormEvent, useState } from "react";
+import { useHistory } from "react-router";
+import styled from "styled-components";
+import { useReduxState, useAppDispatch } from "../hooks";
+import { addError } from "../redux/reducers/errorReducer";
+import { createQuestion } from "../redux/reducers/questions";
 
 export const NewPoll: FC = () => {
-  return <div>New Poll!</div>;
+  const [optionOne, setOptionOne] = useState("");
+  const [optionTwo, setOptionTwo] = useState("");
+  const currentUser = useReduxState(({ currentUser }) => currentUser);
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    let formErrors: string[] = [];
+    if (!optionOne) {
+      formErrors.push("must create first option");
+    }
+    if (!optionTwo) {
+      formErrors.push("must create second option");
+    }
+    if (formErrors.length) {
+      dispatch(addError(formErrors));
+    } else {
+      dispatch(
+        createQuestion({
+          authorId: currentUser.id,
+          optionOneText: optionOne,
+          optionTwoText: optionTwo,
+        })
+      ).then(() => history.push("/"));
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={optionOne}
+        onChange={(e) => setOptionOne(e.currentTarget.value)}
+      />
+      <input
+        type="text"
+        value={optionTwo}
+        onChange={(e) => setOptionTwo(e.currentTarget.value)}
+      />
+      <button style={{ marginTop: 20 }}>Submit</button>
+    </Form>
+  );
 };
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
