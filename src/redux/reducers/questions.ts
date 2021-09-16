d50c40355fa87312612a3706e4ea046a49c013fd
Derrick Hawkins <dhawkvq@yahoo.com>
2021-09-15 22:57:04 -0700

    async thunk createQuestion created and resolved in reducer.

--- src/redux/reducers/questions.ts
+++ src/redux/reducers/questions.ts
@@ -1,11 +1,19 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { questions, QuestionsDb, _saveQuestionAnswer } from "../../DB/_DATA";
+import {
+  questions,
+  QuestionsDb,
+  _saveQuestion,
+  _saveQuestionAnswer,
+} from "../../DB/_DATA";
 import { addError } from "./errorReducer";
 import { setLoadingState } from "./loading";
+import { NewQuestion } from "../../utils";
+import { Question } from "../../types/Question";
+import { addQuestionToUser, saveUserAnswer } from "./allAppUsers";
 
 const initialState: QuestionsDb = { ...questions };
 
-type SaveAnswersArgs = {
+export type SaveAnswersArgs = {
   userId: string;
   questionId: string;
   option: "optionOne" | "optionTwo";
@@ -18,7 +26,26 @@ export const saveAnswer = createAsyncThunk(
     dispatch(setLoadingState(true));
     try {
       await _saveQuestionAnswer({ userId, qid: questionId, option });
+      dispatch(saveUserAnswer(args));
       return args;
+    } catch (error) {
+      const castError = error as Error;
+      dispatch(addError([castError.message]));
+      throw error;
+    } finally {
+      dispatch(setLoadingState(false));
+    }
+  }
+);
+
+export const createQuestion = createAsyncThunk(
+  "CREATE_QUESTION",
+  async (args: NewQuestion, { dispatch }) => {
+    dispatch(setLoadingState(true));
+    try {
+      const savedQuestion = await _saveQuestion(args);
+      dispatch(addQuestionToUser(savedQuestion));
+      return savedQuestion;
     } catch (error) {
       const castError = error as Error;
       dispatch(addError([castError.message]));
@@ -34,24 +61,32 @@ const questionsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(
-      saveAnswer.fulfilled,
-      (state, action: PayloadAction<SaveAnswersArgs>) => {
-        const { userId, questionId, option } = action.payload;
-        const existingQuestion = state[questionId];
-        const existingOption = state[questionId][option];
-        return {
-          ...state,
-          [questionId]: {
-            ...existingQuestion,
-            [option]: {
-              ...existingOption,
-              votes: [...existingOption.votes, userId],
+    builder
+      .addCase(
+        saveAnswer.fulfilled,
+        (state, action: PayloadAction<SaveAnswersArgs>) => {
+          const { userId, questionId, option } = action.payload;
+          const existingQuestion = state[questionId];
+          const existingOption = state[questionId][option];
+          return {
+            ...state,
+            [questionId]: {
+              ...existingQuestion,
+              [option]: {
+                ...existingOption,
+                votes: [...existingOption.votes, userId],
+              },
             },
-          },
-        };
-      }
-    );
+          };
+        }
+      )
+      .addCase(
+        createQuestion.fulfilled,
+        (state, action: PayloadAction<Question>) => ({
+          ...state,
+          [action.payload.id]: action.payload,
+        })
+      );
   },
 });
 
